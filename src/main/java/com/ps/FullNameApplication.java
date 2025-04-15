56262692a0c8378bdd051a416adace3394fcb586
Thierno <podiallo2242@gmail.com>
2025-04-15 13:42:25 -0400

    First Version committed

--- src/main/java/com/ps/FullNameApplication.java
+++ src/main/java/com/ps/FullNameApplication.java
@@ -1,7 +1,49 @@
 package com.ps;
 
+import java.util.Scanner;
+
 public class FullNameApplication {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Full name Generator!");
+
+        System.out.println("Please enter your name: ");
+
+        System.out.print("First name: ");
+        String firstName = scanner.nextLine().trim();
+
+        System.out.print("Middle name: ");
+        String middleName = scanner.nextLine().trim();
+
+        System.out.print("Last name: ");
+        String lastName = scanner.nextLine().trim();
+
+        System.out.print("Suffix: ");
+        String suffix = scanner.nextLine().trim();
+
+        //declare fullName:
+        String fullName = "";
+
+        //let's use the if statement to include only parts that have values
+
+        if(!firstName.isEmpty()){
+            fullName = firstName;
+        }
+
+        if(!middleName.isEmpty()){
+            fullName = firstName + " " + middleName;
+        }
+
+        if(!lastName.isEmpty()){
+            fullName = firstName + " " + middleName + " " + lastName;
+        }
+
+        if(!suffix.isEmpty()){
+            fullName = firstName + " " + middleName + " " + lastName + ","+ suffix;
+        }
+
+        //System.out.println("You full name is: " + fullName);
+        System.out.printf("Your Full name is %s!", fullName);
+
     }
 }
